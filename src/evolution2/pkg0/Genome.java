e3d954ca0abd9a437c294781e4345b0a48921f9f
David <imultiplay123@gmail.com>
2014-05-26 23:34:35 -0400

    Added returnGenomeName and restructured genome in order to have a HashMap of all genomes

--- src/evolution2/pkg0/Genome.java
+++ src/evolution2/pkg0/Genome.java
@@ -12,9 +12,13 @@ import java.util.HashMap;
  *
  * @author david
  *
- * Genome (Length 8)
+ * Genome:
+ * Have a total length of 8, 
+ * but base genome has length of 6
  *
- * First two slots are identifier slots: 00 --> Consume (process) 01 --> Produce
+ * First two slots are identifier slots: 
+ *      00 --> Consume (process) 
+ *      01 --> Produce
  *
  * Energy:
  *
@@ -27,12 +31,15 @@ import java.util.HashMap;
  * Movement:
  *
  * && Add more here &&
+ * 
+ * TODO:
+ * Add file parsing for totalGnome HashMap
  */
 public class Genome {
-
-    ArrayList<String> genome = new ArrayList<>();
-    HashMap<String, String> genomeSorted = new HashMap<>();
-
+    
+    ArrayList<String> genome = new ArrayList<>(); //actual genome list for creature
+    HashMap<String, String> genomeSorted = new HashMap<>(); // translate genome names
+    HashMap<String, String> totalGenome = new HashMap<>(); // hashmap of all knpwn genomes
     public Genome() { // Produce BASIC creature
         fillBasicGenomeSort();
     }
@@ -58,5 +65,20 @@ public class Genome {
         } else {
             return "01" + incompleteGenome.substring(2);
         }
+    }
+    public String returnGenomeName(Genome creature, String genome){
+        String hold = ""; // Find genome identifier and genome name from Total Genome
+        String genomeRoot = genome.substring(2, 8);
+        if (genome.length() > 6){
+            if("01".equals(genome.substring(0, 2))){
+                hold = "Produce" + genomeRoot;
+            } else if ("00".equals(genome.substring(0, 2))) {
+                hold = "Consume" + genomeRoot;
+            }
+        }
+        return hold;
+    }
+    public void fillTotalGenome(){
+        // Make this later
     }
 }
